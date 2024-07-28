e1b4343f9cd75ceac1d89d4ed2b8ad848bec0bcf
Aditya Kalekar <152638244+aadityakalekar@users.noreply.github.com>
2024-07-28 11:23:17 +0530

    Add files via upload

---
+++ SpringSecurityTest/src/main/java/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig  {
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	public UserDetailsService userDetails(PasswordEncoder encoder) {
+//		UserDetails normalUser= User
+//				.withUsername("aditya")
+//				.password(passwordEncoder().encode("password"))
+//				.roles("NORMAL")
+//				.build();
+//		
+//		UserDetails adminUser= User
+//				.withUsername("aditya1")
+//				.password(passwordEncoder().encode("password"))
+//				.roles("ADMIN")
+//				.build();
+//		
+//		return new InMemoryUserDetailsManager(normalUser,adminUser);
+		return new CustomUserDetailsService();
+	}
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+            		.requestMatchers("/home/admin")
+            		.hasRole("ADMIN")
+            		.requestMatchers("/home/normal")
+            		.hasRole("NORMAL")
+                .requestMatchers("/home/public").permitAll()
+                .anyRequest().authenticated()
+            )
+            .formLogin(withDefaults());
+        
+        return httpSecurity.build();
+    }
+}
